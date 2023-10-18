e2fcc38b698023d6131295c8ee6d5dbea8072902
dSp3ar <98909512+dSp3ar@users.noreply.github.com>
2023-10-18 14:24:46 +0300

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,9 @@
-alert(123);
+<PUBLIC:COMPONENT TAGNAME="xss">
+   <PUBLIC:ATTACH EVENT="ondocumentready" ONEVENT="main()" LITERALCONTENT="false"/>
+</PUBLIC:COMPONENT>
+<SCRIPT>
+   function main() 
+   {
+     alert("HTC script executed.");
+   }
+</SCRIPT>
